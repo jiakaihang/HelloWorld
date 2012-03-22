ad4f44736e18dd992ce05ebdb9db0ec9335d8d19
Kaihang Jia <jiakaihang@gmail.com>
2012-03-22 22:51:44 +1100

    third commit

--- HelloWorld/src/test/HelloWorld.java
+++ HelloWorld/src/test/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("This is second commit!");
+		System.out.println("and this is the third commit");
 	}
 
 }
